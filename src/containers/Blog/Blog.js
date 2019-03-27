abc5641678c7f6932e12bcf4b40b3d4e0f3881df
CivilBot <civilbots@gmail.com>
2019-03-27 19:37:38 +0530

    Links got highlighted.
    Active class has been applied to links.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Link} from "react-router-dom";
+import {Route, NavLink} from "react-router-dom";
 
 import './Blog.css';
 import Posts from "../Posts/Posts";
@@ -13,14 +13,19 @@ class Blog extends Component {
                     <nav>
                         <ul>
                             <li>
-                                <Link to="/">Posts</Link>
+                                <NavLink
+                                    exact
+                                    to="/"
+                                    activeClassName="my-active"
+                                    activeStyle={{color: '#fa923f', textDecoration: 'underline'}}
+                                >Posts</NavLink>
                             </li>
                             <li>
-                                <Link to={{
+                                <NavLink to={{
                                     pathname: "/newpost",
                                     search: '?',
                                     hash: '#submit'
-                                }}>New Post</Link>
+                                }}>New Post</NavLink>
                             </li>
                         </ul>
                     </nav>

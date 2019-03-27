bf7896f5752e675515ebb78621e85c49fc3b02cb
clusterwink <36997100+succulentpup@users.noreply.github.com>
2019-03-27 16:04:12 +0530

    Routing added for Posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {Route} from "react-router-dom";
+
 import './Blog.css';
 import Posts from "../Posts/Posts";
 
@@ -16,7 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                {/*<Route path="/" exact render={()=> <h1>Home</h1>}/>*/}
+                <Route path="/" exact component={Posts}/>
             </div>
         );
     }
